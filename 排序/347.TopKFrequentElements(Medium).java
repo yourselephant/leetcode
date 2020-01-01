8924cbdb486d46324d5e7d4a1230382d13628433
yourselephant <jdwd707@gmail.com>
2020-01-01 23:22:39 +0800

    347

---
+++ 排序/347.TopKFrequentElements(Medium).java
@@ -0,0 +1,26 @@
+public List<Integer> topKFrequent(int[] nums, int k) {
+    Map<Integer, Integer> frequencyForNum = new HashMap<>();
+    for (int num : nums) {
+        frequencyForNum.put(num, frequencyForNum.getOrDefault(num, 0) + 1);
+    }
+    List<Integer>[] buckets = new ArrayList[nums.length + 1];
+    for (int key : frequencyForNum.keySet()) {
+        int frequency = frequencyForNum.get(key);
+        if (buckets[frequency] == null) {
+            buckets[frequency] = new ArrayList<>();
+        }
+        buckets[frequency].add(key);
+    }
+    List<Integer> topK = new ArrayList<>();
+    for (int i = buckets.length - 1; i >= 0 && topK.size() < k; i--) {
+        if (buckets[i] == null) {
+            continue;
+        }
+        if (buckets[i].size() <= (k - topK.size())) {
+            topK.addAll(buckets[i]);
+        } else {
+            topK.addAll(buckets[i].subList(0, k - topK.size()));
+        }
+    }
+    return topK;
+}
